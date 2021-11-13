c989b6cac71f3e544e19b0f60eb0accb576a7bb0
Bradley Dowling <brad.dowling@protonmail.com>
2021-11-12 23:49:01 -0500

    html template

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-#! /usr/bin/env pypy3
-# -*- coding: utf-8 -*-
-
 import asyncio
 from quart import Quart
+from quart import render_template
+
 
 app = Quart(__name__)
 
 
 @app.route("/")
-async def index():
-    return "Hello world!"
+@app.route("/hello/")
+@app.route("/hello/<name>")
+async def index(name=None):
+    """Landing page for test application."""
+    return render_template('hello.html', name=name)
 
 
 if __name__ == "__main__":
